98426be6bf6fd517444a61a8bd0b25ba5d9be6cc
andtan <jornandretangen@gmail.com>
2013-05-02 18:10:10 +0200

    favicon ish

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ module.exports = app
 app.configure(function() {
   app.set( 'views', __dirname + '/views' )
   app.set( 'view engine', 'jade' )
-  app.use( express.favicon() )
+  app.use( express.favicon('public/favicon.ico') )
   app.use( express.bodyParser() )
   app.use( express.methodOverride() )
   app.use( app.router )
