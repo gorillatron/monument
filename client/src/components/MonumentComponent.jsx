fcdcc77e4f2c5c97926b493883cf2f122a54dc27
gorillatron <jornandretangen@gmail.com>
2014-08-13 15:37:25 +0200

    test

--- client/src/components/MonumentComponent.jsx
+++ client/src/components/MonumentComponent.jsx
@@ -4,10 +4,23 @@ var _     = require( 'underscore')
 
 var PodCast = React.createClass({
 
+  getInitialState: function() {
+    return { active: false }
+  },
+
+  onClick: function( event ) {
+    this.setState({ active: !this.state.active })
+  },
+
   render: function(){ return (
-    <li className='podcast' style={{ 'background-image': 'url(' +this.props.track.artwork_url.replace('large', 't500x500')+ ')' }}>
-      <div className='title'>
-        <h1> {this.props.track.title} </h1>
+    <li className={'podcast ' + (this.state.active ? 'active' : '')} onClick={this.onClick}>
+      <div className="cover" style={{ 'background-image': 'url(' +this.props.track.artwork_url.replace('large', 't500x500')+ ')' }}>
+      </div>
+      <div className="cover-overlay">
+        <h1 className='title'> {this.props.track.title} </h1>
+        {this.state.active ?
+          <iframe width="100%" height="100%" scrolling="no" frameborder="no" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" +this.props.track.id+ "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"}></iframe> :
+          ''}
       </div>
     </li>
   )}
