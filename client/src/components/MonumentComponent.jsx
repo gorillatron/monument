53d77f5886edb3b133b8218c1a26684191670bff
gorillatron <jornandretangen@gmail.com>
2014-08-13 15:41:07 +0200

    test

--- client/src/components/MonumentComponent.jsx
+++ client/src/components/MonumentComponent.jsx
@@ -19,7 +19,7 @@ var PodCast = React.createClass({
       <div className="cover-overlay">
         <h1 className='title'> {this.props.track.title} </h1>
         {this.state.active ?
-          <iframe width="100%" height="100%" scrolling="no" frameborder="no" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" +this.props.track.id+ "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"}></iframe> :
+          <iframe width="100%" height="100%" scrolling="no" frameborder="no" src={"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" +this.props.track.id+ "&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"}></iframe> :
           ''}
       </div>
     </li>
