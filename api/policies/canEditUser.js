e00a0d7b5ee8b65439c906bc4fc32d90c3379f14
gorillatron <jornandretangen@gmail.com>
2015-03-28 23:52:22 +0100

    no edit for now

--- api/policies/canEditUser.js
+++ api/policies/canEditUser.js
@@ -1,6 +1,11 @@
 
 module.exports = function canEditUser(req, res, next) {
-  
+
+  if(!req.session.user) {
+    console.warn('policy:canEditUser run without any user on session')
+    return res.forbidden('You have to be logged in to edit a user')
+  }
+
   if(req.session.user.role === 'admin') {
     return next()
   }
