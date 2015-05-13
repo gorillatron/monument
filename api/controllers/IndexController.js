26f15480e5cee6b963a46cd81b2eb4bc8227e594
gorillatron <jornandretangen@gmail.com>
2015-05-13 11:21:25 +0200

    fix sorting

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -16,6 +16,27 @@ export default {
 		var [pages, podcasts] = await Promise.all([ Page.find(), Podcast.find() ])
 		var currentPage = {name: 'podcasts'}
 
+		podcasts = podcasts.sort((prev, next) => {
+			let prevNum, nextNum
+
+			try {
+				prevNum = parseInt(
+					prev.soundcloudTrack.title.match(/\d+/)[0]
+				)
+				nextNum = parseInt(
+					next.soundcloudTrack.title.match(/\d+/)[0]
+				)
+				if(isNaN(prevNum) || isNaN(nextNum)) {
+					return 0
+				}
+			}
+			catch(e) {
+				return 0
+			}
+			
+			return nextNum >= prevNum ? 1 : -1
+		})
+
 		return res.view('index', {currentPage, pages, podcasts})
 	}
 
