2ea3e24348df5c41bf9ee9ade4ae312f381bb522
gorillatron <jornandretangen@gmail.com>
2015-06-06 12:56:24 +0200

    import all tracks

--- jobs/importSoundcloudTracks.js
+++ jobs/importSoundcloudTracks.js
@@ -16,7 +16,7 @@ Sails.load({
   var requestTime = (new Date).getTime()
   var insertTime = (new Date).getTime()
 
-  request.get('http://api.soundcloud.com/users/monument-podcast/tracks?client_id=84f2b368d96bdc044dafced637dbce4b&format=json&_status_code_map[302]=200')
+  request.get('http://api.soundcloud.com/users/monument-podcast/tracks?client_id=84f2b368d96bdc044dafced637dbce4b&format=json&_status_code_map[302]=200&limit=999999')
     .then(JSON.parse)
     .then((tracks) => {
       requestTime = (new Date).getTime() - requestTime
