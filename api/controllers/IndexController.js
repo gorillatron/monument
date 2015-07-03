f1401ab0f26feb9ff64d7bba6b49853c273c2e4d
gorillatron <jornandretangen@gmail.com>
2015-07-03 19:33:25 +0200

    event

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -13,31 +13,33 @@ export default {
 
 	index: async function(req, res) {
 
-		var [pages, podcasts] = await Promise.all([ Page.find(), Podcast.find() ])
-		var currentPage = {name: 'podcasts'}
+		return res.redirect('events')
 
-		podcasts = podcasts.sort((prev, next) => {
-			let prevNum, nextNum
-
-			try {
-				prevNum = parseInt(
-					prev.soundcloudTrack.title.match(/\d+/)[0]
-				)
-				nextNum = parseInt(
-					next.soundcloudTrack.title.match(/\d+/)[0]
-				)
-				if(isNaN(prevNum) || isNaN(nextNum)) {
-					return 0
-				}
-			}
-			catch(e) {
-				return 0
-			}
-			
-			return nextNum >= prevNum ? 1 : -1
-		})
-
-		return res.view('index', {currentPage, pages, podcasts})
+		// var [pages, podcasts] = await Promise.all([ Page.find(), Podcast.find() ])
+		// var currentPage = {name: 'events'}
+		//
+		// podcasts = podcasts.sort((prev, next) => {
+		// 	let prevNum, nextNum
+		//
+		// 	try {
+		// 		prevNum = parseInt(
+		// 			prev.soundcloudTrack.title.match(/\d+/)[0]
+		// 		)
+		// 		nextNum = parseInt(
+		// 			next.soundcloudTrack.title.match(/\d+/)[0]
+		// 		)
+		// 		if(isNaN(prevNum) || isNaN(nextNum)) {
+		// 			return 0
+		// 		}
+		// 	}
+		// 	catch(e) {
+		// 		return 0
+		// 	}
+		//
+		// 	return nextNum >= prevNum ? 1 : -1
+		// })
+		//
+		// return res.view('index', {currentPage, pages, podcasts})
 	}
 
 };
